9d142f7f302ff357b6856d0a8702d2ee1d1ff0dd
Yenura <yenura02@gmail.com>
2025-03-19 20:07:50 +0530

    ss

--- stockorra/dataconnect-generated/dart/default_connector/default.dart
+++ stockorra/dataconnect-generated/dart/default_connector/default.dart
@@ -1,16 +1,8 @@
 library default_connector;
+
 import 'package:firebase_data_connect/firebase_data_connect.dart';
-import 'dart:convert';
-
-
-
-
-
-
 
 class DefaultConnector {
-  
-
   static ConnectorConfig connectorConfig = ConnectorConfig(
     'us-central1',
     'default',
@@ -18,13 +10,14 @@ class DefaultConnector {
   );
 
   DefaultConnector({required this.dataConnect});
+
   static DefaultConnector get instance {
     return DefaultConnector(
-        dataConnect: FirebaseDataConnect.instanceFor(
-            connectorConfig: connectorConfig,
-            sdkType: CallerSDKType.generated));
+      dataConnect: FirebaseDataConnect.instanceFor(
+        connectorConfig: connectorConfig, // Removed sdkType
+      ),
+    );
   }
 
   FirebaseDataConnect dataConnect;
 }
-
