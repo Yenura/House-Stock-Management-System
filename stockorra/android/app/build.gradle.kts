240b0ae756bb90fd0844bb0b2d79bfdd277f1d3d
Yenura <yenura02@gmail.com>
2025-03-23 11:24:53 +0530

    start

--- stockorra/android/app/build.gradle.kts
+++ stockorra/android/app/build.gradle.kts
@@ -25,9 +25,8 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.stockorra"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // Updating minSdkVersion to 23 as required by Firebase Auth.
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
